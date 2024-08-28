1e94b6f43f6042f4777a0c070c26777336d173f7
Jason Kreuze <jasonkreuzeschool@gmail.com>
2024-08-28 11:48:05 +0200

    Instructions made and start made to story itself

--- E1_Adventure_Game/Helpers.cs
+++ E1_Adventure_Game/Helpers.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace E1_Adventure_Game
 {
-    internal class Helpers
+    static public class Helpers
     {
+        static public string Ask(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine().ToLower();
+        }
+
+        static public bool GameStart()
+        {
+            Console.WriteLine("Druk op '1' om de game te starten, '2' om de instructies te lezen, '3' om af te sluiten");
+            string input = Console.ReadLine().ToLower();
+            if (input == "1")
+            {
+                return true;
+            }
+            else if (input == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("Spel Instructies");
+                Console.WriteLine("");
+                Console.WriteLine("Je krijgt zodra je de game start een aantal prompts waarin je het verhaal door loopt.");
+                Console.WriteLine("Zodra je een keuze krijgt, kan je kiezen uit een aantal cijfers.");
+                Console.WriteLine("Wat deze opties zijn en wat ze inhouden staat erbij.");
+                Console.WriteLine("");
+                Console.WriteLine("Dat waren alle instructies");
+                Console.WriteLine("");
+                return GameStart();
+            }
+            else if (input == "3")
+            {
+                return false;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Geen geldige input!");
+                Console.WriteLine("");
+                return GameStart();
+            }
+        }
     }
 }
--- E1_Adventure_Game/Program.cs
+++ E1_Adventure_Game/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace AdventureGame
+﻿using E1_Adventure_Game;
+
+namespace AdventureGame
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
             Console.WriteLine("Welkom bij de Backrooms Adventure game");
+            if(Helpers.GameStart())
+            {
+                Console.Clear();
+                string playerName = Helpers.Ask("Voer een naam in: ");
+                Console.WriteLine("Context: Je was hopeloos aan het zoeken naar een baan en hebt gesolliciteerd bij een Jumbo en je bent aangenomen.");
+                Console.WriteLine("Het is tijd voor je eerste dag maar je ziet dat je je verslapen hebt en er over 5 minuten al moet zijn!");
+                Console.WriteLine("Je haast jezelf nog om op tijd te zijn en je haalt het nog net...");
+                Console.WriteLine("");
+                Helpers.Ask("Druk op enter om door te gaan...");
+                Console.Clear();
+                Console.Write("test");
+                Console.WriteLine("test2");
+            }
         }
     }
 }
