79d376ccae1e5e559d2b2de894b83262ff86073e
Jason Kreuze <jasonkreuzeschool@gmail.com>
2024-08-28 10:18:16 +0200

    readded the old files

---
+++ E1_Adventure_Game/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventureGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij de Backrooms Adventure game");
+        }
+    }
+}
+
