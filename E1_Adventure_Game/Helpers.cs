ddcb7b2fc3758e90c734a2b87a770e1deaa7c5d7
Jason Kreuze <jasonkreuzeschool@gmail.com>
2024-09-04 15:37:28 +0200

    Changed Helpers.cs so no if statements are used but switch statements

--- E1_Adventure_Game/Helpers.cs
+++ E1_Adventure_Game/Helpers.cs
@@ -8,44 +8,51 @@ namespace E1_Adventure_Game
 {
     static public class Helpers
     {
-        static public string Ask(string question)
+        // ask function to ask a question and get a response to reduce lines in the future
+        static public string ask(string question)
         {
             Console.Write(question + " ");
             return Console.ReadLine().ToLower();
         }
 
-        static public bool GameStart()
+        // gameStart function to start the game, with the menu.
+        static public bool gameStart()
         {
             Console.WriteLine("Druk op '1' om de game te starten, '2' om de instructies te lezen, '3' om af te sluiten");
-            string input = Console.ReadLine().ToLower();
-            if (input == "1")
+            string userChoice = Console.ReadLine().ToLower();
+            switch (userChoice)
             {
-                return true;
+                case "1":
+                    return true;
+
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("\r\n██╗███╗░░██╗░██████╗████████╗██████╗░██╗░░░██╗░█████╗░████████╗██╗███████╗░██████╗\r\n██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██║░░░██║██╔══██╗╚══██╔══╝██║██╔════╝██╔════╝\r\n██║██╔██╗██║╚█████╗░░░░██║░░░██████╔╝██║░░░██║██║░░╚═╝░░░██║░░░██║█████╗░░╚█████╗░\r\n██║██║╚████║░╚═══██╗░░░██║░░░██╔══██╗██║░░░██║██║░░██╗░░░██║░░░██║██╔══╝░░░╚═══██╗\r\n██║██║░╚███║██████╔╝░░░██║░░░██║░░██║╚██████╔╝╚█████╔╝░░░██║░░░██║███████╗██████╔╝\r\n╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚═════╝░░╚════╝░░░░╚═╝░░░╚═╝╚══════╝╚═════╝░");
+                    Console.WriteLine("");
+                    Console.WriteLine("Je krijgt zodra je de game start een aantal prompts waarin je het verhaal door loopt.");
+                    Console.WriteLine("Zodra je een keuze krijgt, kan je kiezen uit een aantal cijfers.");
+                    Console.WriteLine("Wat deze opties zijn en wat ze inhouden staat erbij.");
+                    Console.WriteLine("");
+                    Console.WriteLine("Dat waren alle instructies");
+                    Console.WriteLine("");
+                    return gameStart();
+
+                case "3":
+                    return false;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Geen geldige input!");
+                    Console.WriteLine("");
+                    return gameStart();
             }
-            else if (input == "2")
-            {
-                Console.Clear();
-                Console.WriteLine("\r\n██╗███╗░░██╗░██████╗████████╗██████╗░██╗░░░██╗░█████╗░████████╗██╗███████╗░██████╗\r\n██║████╗░██║██╔════╝╚══██╔══╝██╔══██╗██║░░░██║██╔══██╗╚══██╔══╝██║██╔════╝██╔════╝\r\n██║██╔██╗██║╚█████╗░░░░██║░░░██████╔╝██║░░░██║██║░░╚═╝░░░██║░░░██║█████╗░░╚█████╗░\r\n██║██║╚████║░╚═══██╗░░░██║░░░██╔══██╗██║░░░██║██║░░██╗░░░██║░░░██║██╔══╝░░░╚═══██╗\r\n██║██║░╚███║██████╔╝░░░██║░░░██║░░██║╚██████╔╝╚█████╔╝░░░██║░░░██║███████╗██████╔╝\r\n╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝░╚═════╝░░╚════╝░░░░╚═╝░░░╚═╝╚══════╝╚═════╝░");
-                Console.WriteLine("");
-                Console.WriteLine("Je krijgt zodra je de game start een aantal prompts waarin je het verhaal door loopt.");
-                Console.WriteLine("Zodra je een keuze krijgt, kan je kiezen uit een aantal cijfers.");
-                Console.WriteLine("Wat deze opties zijn en wat ze inhouden staat erbij.");
-                Console.WriteLine("");
-                Console.WriteLine("Dat waren alle instructies");
-                Console.WriteLine("");
-                return GameStart();
-            }
-            else if (input == "3")
-            {
-                return false;
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Geen geldige input!");
-                Console.WriteLine("");
-                return GameStart();
-            }
+        }
+        
+        // Game Finished Screen.
+        // TODO: Implement ending.
+        static public void gameFinish()
+        {
+            Console.WriteLine("\r\n░██████╗░░█████╗░███╗░░░███╗███████╗  ███████╗██╗███╗░░██╗██╗░██████╗██╗░░██╗███████╗██████╗░\r\n██╔════╝░██╔══██╗████╗░████║██╔════╝  ██╔════╝██║████╗░██║██║██╔════╝██║░░██║██╔════╝██╔══██╗\r\n██║░░██╗░███████║██╔████╔██║█████╗░░  █████╗░░██║██╔██╗██║██║╚█████╗░███████║█████╗░░██║░░██║\r\n██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░  ██╔══╝░░██║██║╚████║██║░╚═══██╗██╔══██║██╔══╝░░██║░░██║\r\n╚██████╔╝██║░░██║██║░╚═╝░██║███████╗  ██║░░░░░██║██║░╚███║██║██████╔╝██║░░██║███████╗██████╔╝\r\n░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═╝╚═════╝░╚═╝░░╚═╝╚══════╝╚═════╝░");
         }
     }
 }
